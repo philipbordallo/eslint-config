c59c09ba83d540e821e9c8c4c36ebc7c1dd3ea1d
Philip Bordallo <git@philipbordallo.com>
2021-03-14 18:23:20 -0400

    Clean up build script

--- scripts/build.js
+++ scripts/build.js
@@ -13,25 +13,30 @@ Mustache.escape = text => text;
 const TEMPLATE = '// THIS IS A GENERATED FILE\nmodule.exports={{ data }};';
 
 /**
- * Builds config file to packages folder
+ * Builds config file (index.js) to packages folder
  * @param {string} file
  */
 async function buildConfig(file) {
   const configFile = path.resolve(CONFIGS_PATH, file);
   const { default: config } = await import(configFile);
 
-  const { longName, shortName } = getPackageName(file);
+  const { longName } = getPackageName(file);
 
   const stringifiedData = JSON.stringify(config);
   const generatedData = TEMPLATE.replace(/{{ data }}/, stringifiedData);
   const exportFile = path.resolve(PACKAGES_PATH, longName, 'index.js');
 
-  fs.writeFileSync(exportFile, generatedData);
-  console.log(`Finished building ${shortName} config to ./packages/${longName}`);
+  fs.promises.writeFile(exportFile, generatedData);
 }
 
+/**
+ * Builds README.md file to packages folder
+ * @param {string} file
+ */
 async function buildReadMe(file) {
-  const template = fs.readFileSync(path.resolve(SRC_PATH, 'README.template.md'), { encoding: 'utf-8' });
+  const template = await fs.promises.readFile(
+    path.resolve(SRC_PATH, 'README.template.md'), { encoding: 'utf-8' },
+  );
   const { longName, shortName } = getPackageName(file);
 
   const packageFile = path.resolve(PACKAGES_PATH, longName, 'package.json');
@@ -48,10 +53,16 @@ async function buildReadMe(file) {
   const output = Mustache.render(template, view);
   const readMeFile = path.resolve(PACKAGES_PATH, longName, 'README.md');
 
-  fs.writeFileSync(readMeFile, output);
+  fs.promises.writeFile(readMeFile, output);
 }
 
 readConfigs((file) => {
-  buildConfig(file);
-  buildReadMe(file);
+  const { longName, shortName } = getPackageName(file);
+
+  Promise.all([
+    buildConfig(file),
+    buildReadMe(file),
+  ]).then(() => {
+    console.log(`Finished building ${shortName} config to ./packages/${longName}`);
+  });
 });
